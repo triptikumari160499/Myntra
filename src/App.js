cf59f1d2009e1e4fd84f0f9da518b9d3cddc748d
Anshul044 <anshuljindal270411@gmail.com>
2021-04-02 18:19:44 +0530

    header gone

--- src/App.js
+++ src/App.js
@@ -7,33 +7,7 @@ class App extends React.Component
     return (
       <div>
         <header id="header">
-          <div class="header_top">
-            <div class="container">
-              <div class="row">
-                <div class="col-sm-6">
-                  <div class="contactinfo">
-                    <ul class="nav nav-pills">
-                      <li><a href="#"><i class="fa fa-phone"></i> 1414141414</a></li>
-                      <li><a href="#"><i class="fa fa-envelope"></i> info@domain.com</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="social-icons pull-right">
-                    <ul class="nav navbar-nav">
-                      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                      <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                      <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                      <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-          <div class="header-middle">
+          <div>
             <div class="container">
               <div class="row">
                 <div class="col-sm-4">
