e32481639b78c117814691f831716da94e1f0fbc
triptikumari160499 <triptikumari160499@gmail.com>
2021-04-08 12:09:46 +0530

    table_image

--- src/Table.js
+++ src/Table.js
@@ -1,39 +1,72 @@
 import React from "react";
 import {Table} from "antd";
 import 'antd/dist/antd.css';
-const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-  ];
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+  render: (text, record) => {
+     return (
+      <div>
+      <img src={record.productimage}/>
+      {/*<Avatar src={record.productimage}/> */}
+        <div>
+         <div>{record.productnumber}</div>
+         <a href="javascript:alert('ciao');">{record.productname}</a>
+         </div>
+      </div>
+    );},
+  }, 
+ {
+  title: 'Address',
+  dataIndex: 'address', 
+  key: 'address',  
+ },
+ {
+  title: 'age',
+  dataIndex: 'age', 
+  key: 'age',
+ },
+ 
+ ];
+
+const data = [{
+  key: '1',
+  productimage:'https://img.icons8.com/dusk/2x/chart.png',
+  productnumber:'123456-1234-56',
+  productname:'AAAAA',
+  address: 'New York No. 1 Lake Park',
+  age: 32,
+  Tags: ['M', 'Blue'],
+  }, 
+  {
+  key: '2',
+  productimage:'https://img.icons8.com/dusk/2x/customer-insight.png',
+  productnumber:'123456-1234-56',
+  productname:'AAAAA',
+  address: 'New York No. 1 Lake Park',
+  age: 32,
+  tags: ['XL', 'orange'],
+  }, 
+  {
+  key: '3',
+  productimage:'https://img.icons8.com/dusk/2x/barcode-scanner.png',
+  productnumber:'123456-1234-56',
+  productname:'AAAAA',
+  address: 'New York No. 1 Lake Park',
+  age: 32,
   
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-  ];
+  }, 
+  {
+  key: '4',
+  productimage:'https://img.icons8.com/dusk/2x/squared-menu.png',
+  productnumber:'123456-1234-56',
+  productname:'AAAAA',
+  address: 'New York No. 1 Lake Park',
+  age: 32,
+}];
+
   const Tables=()=>{
-    return <Table dataSource={dataSource} columns={columns} />;
+    return <Table dataSource={data} columns={columns} />;
   }
   export default Tables;
