6a6f9e8d8f5ebc1faac6254088529825f59a4aec
khushbooJangle <khushboojangle@gmail.com>
2021-04-09 11:30:50 +0530

    Done Styling

--- src/Components/Body/Explore Section/ExploreSection.js
+++ src/Components/Body/Explore Section/ExploreSection.js
@@ -7,9 +7,13 @@ const ExploreSection = (props) => {
     <div>
       <h1 id="g">{props.title} </h1>
       <div id="sports">
-        {sectionImages.map((i, key) => (
-          <ExploreCard image={i} />
-        ))}
+        <div class="container-fluid mb-4">
+          <div class="row justify-content-center row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+            {sectionImages.map((i, key) => (
+              <ExploreCard image={i} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
