042c77d06845b0332e555860a198bd4cbfe0f730
Anshul044 <anshuljindal270411@gmail.com>
2021-04-21 14:29:30 +0530

    Chat Header

--- src/Components/Chat/ChatFeed.jsx
+++ src/Components/Chat/ChatFeed.jsx
@@ -91,9 +91,7 @@ const ChatFeed = (props) => {
     <div className="chat-feed">
       <div className="chat-title-container">
         <div className="chat-title">{chat?.title}</div>
-        <div className="chat-subtitle">
-          {chat.people.map((person) => `${person.person.username}`)}
-        </div>
+        
       </div>
       {renderMessages()}
       {/* {chat?.id == data.chat_id ? (
